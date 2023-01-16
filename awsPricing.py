2513dfd93e6a2b7b0baaebf24a2bfb74ca79962b
sunsetmountain <brandon@sunsetmountain.com>
2023-01-16 09:45:42 -0700

    Update awsPricing.py

--- awsPricing.py
+++ awsPricing.py
@@ -12,85 +12,85 @@ def main():
     offer_index_url = "/offers/v1.0/aws/index.json"
     request_url = base_url + offer_index_url
     offerURLs = readOfferIndexFile(request_url)
-
     number_of_offers = 0
     for i in offerURLs:
         number_of_offers += 1
         readOfferFile(i, base_url + offerURLs[i])
-
     print(f"Number of offerings: {number_of_offers}")
 
 def readOfferIndexFile(url):
     response = requests.get(url)
-
     # Create a dict to store offer list
     offeritems= {}
-
     # Add the retail prices returned in the API response to a list
     for i in response.json()['offers']:
         offer_url = response.json()['offers'][i]['currentVersionUrl']
         offeritems[i]=offer_url
-
     return offeritems
 
 def readOfferFile(offering, url):
-    # Set the file location and filename to save files
-    filelocation = str(Path(__file__).parent.parent) + "/awsPricing/products/"
-    base_filename = 'awsProducts-'
-    offeringFilename = base_filename + offering
-    termsFilename = base_filename + "Terms" + offering
-
     print(f"{offering}:{url}")
     response = requests.get(url)
+    readProducts(offering, response)
+    readTerms(offering, response)
 
+def readProducts(offering, response):
+    # Set the file location and filename to save files
+    filelocation = str(Path(__file__).parent) + "/awsPricing/products/"
+    filename = 'awsProducts-' + offering
     # Create arrays to store products and terms
     productitems = []
-    termitems = {}
-
     productsJSON = response.json()['products']
     for i in productsJSON:
-        productitems.append(productsJSON[i])
-
-    termsJSON = response.json()['terms']
-    # using keys example - https://stackoverflow.com/questions/34327719/get-keys-from-json-in-python#34327880
-    terms = termsJSON.keys()
-    for term in terms:
-        #termitems.append('term':term)
-        products = termsJSON[term].keys()
-        for product in products:
-            offerTerms = termsJSON[term][product].keys()
-            for offerTerm in offerTerms:
-                pricedimensions = termsJSON[term][product][offerTerm].keys()
-                if type(termsJSON[term][product][offerTerm]) == dict:
-                    print(f"OfferTerm Dict found: {(termsJSON[term][product][offerTerm])}")
-                else:
-                    print(f"OfferTerm Key pair found: {(termsJSON[term][product][offerTerm])}")
-                for pricedimension in pricedimensions:
-                    if type(termsJSON[term][product][offerTerm][pricedimension]) == dict:
-                        print(f"PriceDimension Dict found: {(termsJSON[term][product][offerTerm][pricedimension])}")
-                    else:
-                        print(f"PriceDimension Key pair found: {(termsJSON[term][product][offerTerm][pricedimension])}")
-                    #print(product)
-                    #print(termsJSON[term][product])
-                    #termitems.append(termsJSON[term][product][pricedimension])
-
+        productitems.append(get_simple_keys(productsJSON[i]))
     #flatten out the json
-    #dfProducts = pd.json_normalize(productitems)
-    dfTerms = pd.json_normalize(termitems)
-
-    print(dfTerms)
+    dfProducts = pd.read_json(json.dumps(productitems))
 
     #shorten the header names
-    #for col in dfProducts.columns:
-    #    colName = col
-    #   dotPosition = colName.rfind(".")
-    #   if dotPosition > -1:
-    #       newName = colName[dotPosition+1:]
-    #       dfProducts.rename({colName: newName}, axis=1, inplace=True)
+    for col in dfProducts.columns:
+        colName = col
+        dotPosition = colName.rfind(".")
+        if dotPosition > -1:
+            newName = colName[dotPosition+1:]
+            dfProducts.rename({colName: newName}, axis=1, inplace=True)
+    # Save the data frame as a CSV file
+    dfProducts.to_csv(os.path.join(filelocation,filename) + '.csv', index=False)
 
-    ## Save the data frame as a CSV file
-    ##dfProducts.to_csv(os.path.join(filelocation,offeringFilename) + '.csv', index=False)
-    dfTerms.to_csv(os.path.join(filelocation,termsFilename) + '.csv', index=False)
+def readTerms(offering, response):
+    # Set the file location and filename to save files
+    filelocation = str(Path(__file__).parent) + "/awsPricing/products/"
+    filename = 'awsTerms-' + offering
+    # Create arrays to store products and terms
+    items = ""
+    termsJSON = response.json()['terms']
+    # print(termsJSON)
+    # sys.exit()
+    terms = termsJSON.keys()
+    result = []
+    for term in terms:
+        products = termsJSON[term].keys()
+        for product in products:
+            productItems = get_simple_keys(termsJSON[term][product])
+            productItems['terms'] = term
+            result.append(productItems)
+    jsonString = json.dumps(result)
+    #read the JSON into Pandas
+    dfTerms = pd.read_json(jsonString)
+    # Save the data frame as a CSV file
+    dfTerms.to_csv(os.path.join(filelocation,filename) + '.csv', index=False)
+
+def get_simple_keys(data, result={}):
+    #from https://stackoverflow.com/questions/34327719/get-keys-from-json-in-python#34327880
+    for key in data.keys():
+        if type(data[key]) not in [dict, list]:
+            result[key] = data[key]
+            print(result)
+        elif type(data[key]) == list:
+            continue
+        else:
+            #print(f"dict data type - key: {key} value: {data[key]}")
+            get_simple_keys(data[key])
+    return result
 
 if __name__ == "__main__":
     main()
